64fd0b307ae719338699942b7eb363c2c6a3f6fe
ixr230014 <ixr230014@utdallas.edu>
2025-12-28 16:31:46 -0600

    testing two

--- swag.cpp
+++ swag.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
-#include <string>
+#include <array>
+#include <algorithm>
 
 int main(void){
 
-    int x = 7;
-    std::cout << x << std::endl;
-    x = 3;
-    std::cout << x << std::endl;
-    
+    /*
+    int arr[] = {1,3,5};
+    std::array<int,3> arr2{1,3,5};
+
+    //C-style for loop
+    for(int i=0; i<arr2.size(); i=i+1){
+        std::cout << arr2[i] << std::endl;
+    }
+
+    //range-based for loops
+    for(int element: arr2){
+        std::cout << element << std::endl;
+    }
+*/
+    std::array<int,3> myArray;
+    std::fill(std::begin(myArray), std::end(myArray), 1024);
+
+    for(int element: myArray){
+        std::cout << element << std::endl;
+    }
+
     return 0;
 }
