c4172f8507af93f64a1e30072cbebd6b82f5425f
ixr230014_desktop <ixr230014@utdallas.edu>
2026-01-15 23:12:59 -0600

    cool

--- shah.cpp
+++ shah.cpp
@@ -1,32 +1,6 @@
 #include <iostream>
 
-void foo(){
-
-}
-
-int main(){
-
-    int x = 42;
-    float y = 72;
-    char a = 'a';
-    signed char b = 'b';
-    unsigned char c = 'c';
-
-    std::cout << "x: " << &x << std::endl;
-    std::cout << "x's size: " << sizeof(x) << std::endl;
-
-    std::cout << "y: " << &y << std::endl;
-    std::cout << "y's size: " << sizeof(y) << std::endl;
-
-    std::cout << "a: " << &a << std::endl;
-    std::cout << "y\a's size: " << sizeof(a) << std::endl;
-
-    std::cout << "b: " << &b << std::endl;
-    std::cout << "b's size: " << sizeof(b) << std::endl;
-
-    std::cout << "c: " << &c << std::endl;
-    std::cout << "c's size: " << sizeof(c) << std::endl;
-
-
-    return 0;
+void SetValue(int arg){
+    std::cout << "agr's addr is : " << &arg << std::endl;
+    arg = 9999;
 }
